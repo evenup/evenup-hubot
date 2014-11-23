4d87b58af68c14a6a64571cc80a4b852262dedb8
Isaias Piña <iparjona@gmail.com>
2014-11-23 08:16:59 -0600

    Fixes to pass puppet lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -20,27 +20,27 @@ class hubot::install {
   }
 
   group { 'hubot':
-    ensure  => present,
-    system  => true,
+    ensure => present,
+    system => true,
   }
 
   user { 'hubot':
-    ensure      => present,
-    comment     => 'Hubot Service User',
-    system      => true,
-    gid         => 'hubot',
-    home        => $::hubot::root_dir,
-    shell       => '/bin/bash',
-    managehome  => true,
-    require     => Group['hubot'],
+    ensure     => present,
+    comment    => 'Hubot Service User',
+    system     => true,
+    gid        => 'hubot',
+    home       => $::hubot::root_dir,
+    shell      => '/bin/bash',
+    managehome => true,
+    require    => Group['hubot'],
   }
 
   file { $::hubot::root_dir:
-    ensure   => directory,
-    owner    => 'hubot',
-    group    => 'hubot',
-    mode     => '0644',
-    require  => User['hubot'],
+    ensure  => directory,
+    owner   => 'hubot',
+    group   => 'hubot',
+    mode    => '0644',
+    require => User['hubot'],
   }
 
   if $::hubot::build_deps {
@@ -55,12 +55,12 @@ class hubot::install {
   }
 
   package { [$hubot_to_install, 'coffee-script']:
-    ensure    => 'installed',
-    require   => [
-                   User['hubot'],
-                   Package[$::hubot::build_deps],
-                 ],
-    provider  => 'npm',
-    notify    => Class['hubot::config'],
+    ensure   => 'installed',
+    require  => [
+                  User['hubot'],
+                  Package[$::hubot::build_deps],
+    ],
+    provider => 'npm',
+    notify   => Class['hubot::config'],
   }
 }
